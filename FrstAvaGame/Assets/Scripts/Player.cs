322bf7a713cb0c749810a8bdeeb6147202510416
WilliCarvalho <willian_matheus14@hotmail.com>
2020-01-15 19:49:21 -0300

    Initial Commit
    Primeiro Commit com alguns Scripts essenciais para que se possa dar inicio na criação do jogo

---
+++ FrstAvaGame/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : PlayerPreset
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimentos dos personagens
+        float moveX = Input.GetAxisRaw("Horizontal") * velocity * Time.deltaTime;
+        float moveY = Input.GetAxisRaw("Vertical") * velocity * Time.deltaTime;
+        transform.Translate(moveX, moveY, 0.0f);
+    }
+}
---
+++ FrstAvaGame/Assets/Scripts/PlayerPreset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPreset : MonoBehaviour
+{
+    //Preset para colocar nos personagens, inimigos, etc.
+
+    public GameObject player;
+    public SpriteRenderer spriteRenderer;
+    public Rigidbody2D rigidbody;
+    public Animator animator;
+    public float velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
